f676d64a010f5a09e236d6c6205c478b89b35183
Nathan Newyen <nathan@nathannewyen.com>
2024-05-03 02:50:27 -0400

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'expenses_list.dart';
+import 'models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 15.99, date: DateTime.now(), category: Category.leisure)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
+        ],
+      ),
+    );
+  }
+}
