4ea64cddb3ae4220bd033adfb7ffac0ec1b949fc
Nathan Newyen <nathan@nathannewyen.com>
2024-05-05 11:17:43 -0400

    added dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,30 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -17,8 +38,8 @@ void main() {
         cardTheme: const CardTheme().copyWith(
           color: kColorScheme.secondaryContainer,
           margin: const EdgeInsets.symmetric(
-            vertical: 16,
-            horizontal: 8,
+            vertical: 8,
+            horizontal: 16,
           ),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
@@ -28,12 +49,13 @@ void main() {
         ),
         textTheme: ThemeData().textTheme.copyWith(
           titleLarge: TextStyle(
-            fontSize: 14,
+            fontSize: 16,
             color: kColorScheme.onSecondaryContainer,
-            fontWeight: FontWeight.normal,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
+      themeMode: ThemeMode.dark,
       home: const Expenses(),
     )
   );
